70484c7a82ef495fd3adffde341f2025e830c4d2
ethan-hatcher <166641457+ethan-hatcher@users.noreply.github.com>
2025-04-20 16:16:28 -0400

    Update musicData.h

--- musicData.h
+++ musicData.h
@@ -46,6 +46,105 @@ public:
         }
     }
 
+    void merge(std::vector<song>& songs, int L, int R, int Mid, string P) {
+        if(P == "popularity") {
+            int sizeL = Mid - L + 1;
+            int sizeR = R - Mid;
+
+            vector<song> tempA(songs.begin() + L, songs.begin() + Mid + 1);
+            vector<song> tempB(songs.begin() + Mid + 1, songs.begin() + R + 1);
+
+            int i,j,k;
+            i=0;
+            j=0;
+            k=L;
+
+            while(i < sizeL && j < sizeR) {
+                if(tempA[i].popularity > tempB[j].popularity) {
+                    songs[k] = tempA[i];
+                    i++;
+                }
+                else {
+                    songs[k] = tempB[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while(i < sizeL) {
+                songs[k] = tempA[i];
+                i++;
+                k++;
+            }
+            while(j < sizeR) {
+                songs[k] = tempB[j];
+                j++;
+                k++;
+            }
+
+        } else if(P == "liveness") {
+            int sizeL = Mid - L + 1;
+            int sizeR = R - Mid;
+
+            vector<song> tempA(songs.begin() + L, songs.begin() + Mid + 1);
+            vector<song> tempB(songs.begin() + Mid + 1, songs.begin() + R + 1);
+
+            int i,j,k;
+            i=0;
+            j=0;
+            k=L;
+
+            while(i < sizeL && j < sizeR) {
+                if(tempA[i].liveness > tempB[j].liveness) {
+                    songs[k] = tempA[i];
+                    i++;
+                }
+                else {
+                    songs[k] = tempB[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while(i < sizeL) {
+                songs[k] = tempA[i];
+                i++;
+                k++;
+            }
+            while(j < sizeR) {
+                songs[k] = tempB[j];
+                j++;
+                k++;
+            }
+        } else {
+            return;
+        }
+    }
+
+    void mergeSort(std::vector<song>& songs, int L, int R, string P) {
+        if(P == "popularity") {
+            if(L >= R) {
+                return;
+            }
+            int Mid = L + (R - L) / 2;
+            mergeSort(songs, L, Mid, "popularity");
+            mergeSort(songs, Mid + 1, R, "popularity");
+            merge(songs, L, Mid, R);
+
+        } else if(P == "liveness") {
+            if(L >= R) {
+                return;
+            }
+            int Mid = L + (R - L) / 2;
+            mergeSort(songs, L, Mid, "liveness");
+            mergeSort(songs, Mid + 1, R, "liveness");
+            merge(songs, L, Mid, R);
+
+        } else {
+            return;
+        }
+    }
+
 };
 
 #endif //MUSICDATA_H
