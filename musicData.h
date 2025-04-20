03a90a65d57a2b265f1b3311813eb8a4d9648b7b
CalebDandeneau <calebdandeneau@gmail.com>
2025-04-20 16:01:02 -0400

    Implemented quicksort for both popularity and liveness, added a default constructor to song struct since it was causing issues with running main, added 2 tests in main to ensure quick sort worked.

--- musicData.h
+++ musicData.h
@@ -11,6 +11,8 @@
 #define MUSICDATA_H
 
 struct song {
+    //Added a default constructor to song
+    song() = default;
     std::string artistName;
     std::string trackName;
     std::string trackGenre;
@@ -18,34 +20,113 @@ struct song {
     float danceability;
     float liveness;
 
-    song(std::string artistName, std::string trackName, std::string trackGenre, float popularity, float danceability, float liveness) {
-        this->artistName = artistName;
-        this->trackName = trackName;
-        this->trackGenre = trackGenre;
-        this->popularity = popularity;
-        this->danceability = danceability;
-        this->liveness = liveness;
-    }
+    song(const std::string &artistName,
+         const std::string &trackName,
+         const std::string &trackGenre,
+         float popularity,
+         float danceability,
+         float liveness)
+        : artistName(artistName),
+          trackName(trackName),
+          trackGenre(trackGenre),
+          popularity(popularity),
+          danceability(danceability),
+          liveness(liveness)
+    {}
 };
 
-class MusicData{
-private:
+class MusicData {
 public:
     std::vector<song> data;
+
     MusicData() {
         csv::CSVReader reader("music.csv");
-        for (csv::CSVRow& row : reader) {
-            std::string artistName = row["artists"].get<>();
-            std::string trackName = row["track_name"].get<>();
-            std::string trackGenre = row["track_genre"].get<>();
-            float popularity = row["popularity"].get<float>();
-            float danceability = row["danceability"].get<float>();
-            float liveness = row["liveness"].get<float>();
-
-            data.emplace_back(artistName, trackName, trackGenre, popularity, danceability, liveness);
+        for (csv::CSVRow &row : reader) {
+            data.emplace_back(
+                row["artists"].get<>(),
+                row["track_name"].get<>(),
+                row["track_genre"].get<>(),
+                row["popularity"].get<float>(),
+                row["danceability"].get<float>(),
+                row["liveness"].get<float>()
+            );
         }
     }
+//2 Quick sort functions, one for sorting by liveness, the other for popularity
+    std::vector<song> quick_sort_by_liveness(int N) {
+        std::vector<song> arr = data;
+        quick_sort_liveness(arr.begin(), arr.end());
+        if ((int)arr.size() > N) arr.resize(N);
+        return arr;
+    }
 
+    std::vector<song> quick_sort_by_popularity(int N) {
+        std::vector<song> arr = data;
+        quick_sort_popularity(arr.begin(), arr.end());
+        if ((int)arr.size() > N) arr.resize(N);
+        return arr;
+    }
+
+private:
+    //template to allow any type of data to be passed through
+    //both versions of sort use median of three method to choose a pivot
+    template<typename RandomIt>
+    void quick_sort_liveness(RandomIt first, RandomIt last) {
+        if (last - first < 2) return;
+        auto midIt = first + (last - first) / 2;
+        song pivot = *first;
+        song midElem = *midIt;
+        song lastElem = *(last - 1);
+        if (first->liveness > midElem.liveness) {
+            if (midElem.liveness > lastElem.liveness) pivot = midElem;
+            else if (first->liveness > lastElem.liveness) pivot = lastElem;
+            else pivot = *first;
+        } else {
+            if (first->liveness > lastElem.liveness) pivot = *first;
+            else if (midElem.liveness > lastElem.liveness) pivot = lastElem;
+            else pivot = midElem;
+        }
+        RandomIt left = first, right = last - 1;
+        while (left <= right) {
+            while (left->liveness > pivot.liveness) ++left;
+            while (pivot.liveness > right->liveness) --right;
+            if (left <= right) {
+                std::iter_swap(left, right);
+                ++left; --right;
+            }
+        }
+        quick_sort_liveness(first, right + 1);
+        quick_sort_liveness(left, last);
+    }
+
+    template<typename RandomIt>
+    void quick_sort_popularity(RandomIt first, RandomIt last) {
+        if (last - first < 2) return;
+        auto midIt = first + (last - first) / 2;
+        song pivot = *first;
+        song midElem = *midIt;
+        song lastElem = *(last - 1);
+        if (first->popularity > midElem.popularity) {
+            if (midElem.popularity > lastElem.popularity) pivot = midElem;
+            else if (first->popularity > lastElem.popularity) pivot = lastElem;
+            else pivot = *first;
+        } else {
+            if (first->popularity > lastElem.popularity) pivot = *first;
+            else if (midElem.popularity > lastElem.popularity) pivot = lastElem;
+            else pivot = midElem;
+        }
+        RandomIt left = first, right = last - 1;
+        while (left <= right) {
+            while (left->popularity > pivot.popularity) ++left;
+            while (pivot.popularity > right->popularity) --right;
+            if (left <= right) {
+                std::iter_swap(left, right);
+                ++left; --right;
+            }
+        }
+        quick_sort_popularity(first, right + 1);
+        quick_sort_popularity(left, last);
+    }
 };
 
-#endif //MUSICDATA_H
+#endif // MUSICDATA_H
