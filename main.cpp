9cf618ef5ef152bbd942e4b3d3a930a7447a0400
CalebDandeneau <calebdandeneau@gmail.com>
2025-04-21 22:22:05 -0400

    New Experimental Branch, adds the ability to sort by danceability and duration, also adds the ability to choose if you want your parameter to come out in descending or ascedning order. Seems to have fixed the issue with time complexity between merge and quick sort but more testing needs to be done to confirm

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
+#include <chrono>
 #include "musicData.h"
 
 int main() {
     MusicData musicData;
     std::string genre;
     std::string sortParameter;
+    std::string sortOrder;
     int numberOfQueriedSongs;
 
-    std::unordered_set<std::string> availableGenres = {"world-music","turkish","trance","synth-pop","swedish",
+    std::unordered_set<std::string> availableGenres = {
+        "world-music","turkish","trance","synth-pop","swedish",
         "songwriter","ska","singer-songwriter","show-tunes","sertanejo","sad","romance","rockabilly",
         "rock-n-roll","reggaeton","r-n-b","psych-rock","salsa","progressive-house","power-pop","pop",
         "samba","party","pagode","new-age","minimal-techno","pop-film","metalcore","metal","mandopop",
@@ -20,129 +24,140 @@ int main() {
         "anime","bluegrass","edm","chill","punk-rock","funk","black-metal","electro","trip-hop",
         "dancehall","breakbeat","electronic","emo","hard-rock","soul","j-idol","garage","folk",
         "groove","heavy-metal","german","french","gospel","grindcore","jazz","happy","hardstyle",
-        "chicago-house","hip-hop","punk","honky-tonk","house","industrial","indian"};
+        "chicago-house","hip-hop","punk","honky-tonk","house","industrial","indian"
+    };
 
-    std::cout<<"Welcome to the song Ranker"<<std::endl;
-    std::cout<<"Enter a Genre. To skip, press enter, to see a list of available genres, enter \"list\""<<std::endl;
+    std::cout << "Welcome to the Song Sort-Scovery\n";
+    std::cout << "Enter a Genre. To skip, press enter, to see a list of available genres, enter \"list\"\n";
     std::getline(std::cin, genre);
-    // genre can be "/n" or "[valid genre]"
-
-    // get genre from user
-    while(true) {
-        if (genre == "") {
-            break;
-        } else if(genre == "list") {
-            for(auto it = availableGenres.begin(); it != availableGenres.end(); it++) {
-                std::cout<<*it<<std::endl;
-            }
-            std::cout<<"Enter a Genre. To skip, press enter, to see a list of available genres, enter \"list\""<<std::endl;
-        } else if(availableGenres.find(genre) == availableGenres.end()) {
-            std::cout<<"Genre does not exist. To skip, press enter, to see a list of available genres, enter \"list\""<<std::endl;
-        } else {
-            break;
-        }
+    while (true) {
+        if (genre.empty()) break;
+        if (genre == "list") {
+            for (auto &g : availableGenres) std::cout << g << "\n";
+            std::cout << "Enter a Genre… or \"list\" again:\n";
+        } else if (!availableGenres.count(genre)) {
+            std::cout << "Genre does not exist. Try again or press enter to skip:\n";
+        } else break;
         std::getline(std::cin, genre);
     }
 
-    // get songs that fit constraints provided by user
-    std::vector<song> constrainedSongs;
-    if(genre != "") {
-        for(song song : musicData.data) {
-            if(song.trackGenre == genre) {
-                constrainedSongs.push_back(song);
 
-            }
-        }
+    std::vector<song> constrainedSongs;
+    if (!genre.empty()) {
+        for (auto &s : musicData.data)
+            if (s.trackGenre == genre)
+                constrainedSongs.push_back(s);
     } else {
         constrainedSongs = musicData.data;
     }
 
-    // get sort parameter from user
-    std::cout<<"Do you want to sort by popularity or liveness?"<<std::endl;
-    while(true) {
+
+    std::cout << "Sort by popularity, danceability, liveness or duration?\n";
+    while (true) {
         std::getline(std::cin, sortParameter);
-        if (sortParameter == "popularity" || sortParameter == "liveness") {
-            break;
+        if (sortParameter == "popularity"
+         || sortParameter == "danceability"
+         || sortParameter == "liveness"
+         || sortParameter == "duration") break;
+        std::cout << "Invalid. Choose: popularity, danceability, liveness, duration.\n";
+    }
+
+
+    std::cout << "Sort by most or the least " << sortParameter << "?\n";
+    std::cout << "Note: Selecting most prints the highest values of your sorting choice first\n";
+    while (true) {
+        std::getline(std::cin, sortOrder);
+        if (sortOrder == "most" || sortOrder == "least") break;
+        std::cout << "Invalid. Enter \"most\" or \"least\":\n";
+    }
+
+
+    std::cout << "How many songs do you want?\n";
+    while (true) {
+        std::string tmp;
+        std::getline(std::cin, tmp);
+        numberOfQueriedSongs = std::stoi(tmp);
+        if (numberOfQueriedSongs > 0) break;
+        std::cout << "Enter an integer greater than 0.\n";
+    }
+
+    std::cout << "\nloading...\n\n";
+    std::vector<song> quickResults, mergeResults;
+    std::chrono::duration<double> qt, mt;
+
+
+    {
+        auto t0 = std::chrono::high_resolution_clock::now();
+        if (sortOrder == "most") {
+            quickResults = musicData.quick_sort_subset(
+                constrainedSongs,
+                sortParameter,
+                numberOfQueriedSongs
+            );
         } else {
-            std::cout<<"Invalid Entry. Do you want to sort by popularity or liveness?"<<std::endl;
+            auto fullDesc = musicData.quick_sort_subset(
+                constrainedSongs,
+                sortParameter,
+                (int)constrainedSongs.size()
+            );
+            std::vector<song> bottom(
+                fullDesc.end() - numberOfQueriedSongs,
+                fullDesc.end()
+            );
+            quickResults = { bottom.rbegin(), bottom.rend() };
         }
+        qt = std::chrono::high_resolution_clock::now() - t0;
     }
 
-    // get number of songs to return
-    while(true) {
-        std::cout<<"How many songs do you want?"<<std::endl;
-        std::string temp;
-        std::getline(std::cin, temp);
-        numberOfQueriedSongs = std::stoi(temp);
-        if(numberOfQueriedSongs > 0) {
-            break;
+
+    {
+        auto t0 = std::chrono::high_resolution_clock::now();
+        auto fullMerge = musicData.mergeSort(constrainedSongs, sortParameter);
+        if (sortOrder == "most") {
+            mergeResults = fullMerge;
+            if ((int)mergeResults.size() > numberOfQueriedSongs)
+                mergeResults.resize(numberOfQueriedSongs);
+        } else {
+            std::vector<song> bottom(
+                fullMerge.end() - numberOfQueriedSongs,
+                fullMerge.end()
+            );
+            mergeResults = { bottom.rbegin(), bottom.rend() };
         }
-        std::cout<<"Enter an integer greater than 0."<<std::endl;
-    }
-    std::cout<<"loading..."<<std::endl;
-
-    // call sort functions here
-    std::vector<song> quickResults;
-    std::vector<song> mergeResults;
-    std::chrono::duration<double> quicksortTime;
-    std::chrono::duration<double> mergesortTime;
-
-    if (sortParameter == "popularity") {
-        auto start = std::chrono::high_resolution_clock::now();
-        quickResults = musicData.quick_sort_by_popularity(numberOfQueriedSongs);
-        auto end = std::chrono::high_resolution_clock::now();
-        quicksortTime = end - start;
-
-        start = std::chrono::high_resolution_clock::now();
-        mergeResults = musicData.mergeSort(constrainedSongs, sortParameter);
-        end = std::chrono::high_resolution_clock::now();
-        mergesortTime = end - start;
-
-    } else if(sortParameter == "liveness") {
-        auto start = std::chrono::high_resolution_clock::now();
-        quickResults = musicData.quick_sort_by_liveness(numberOfQueriedSongs);
-        auto end = std::chrono::high_resolution_clock::now();
-        quicksortTime = end - start;
-
-        start = std::chrono::high_resolution_clock::now();
-        mergeResults = musicData.mergeSort(constrainedSongs, sortParameter);
-        end = std::chrono::high_resolution_clock::now();
-        mergesortTime = end - start;
+        mt = std::chrono::high_resolution_clock::now() - t0;
     }
 
-    std::cout<<"Top " << numberOfQueriedSongs << " songs ";
 
-    if (genre != "") {
-        std::cout<<"in the "<< genre<<" genre ";
+    std::cout << "Top " << numberOfQueriedSongs
+              << (genre.empty() ? " songs " : " songs in the \"" + genre + "\" genre ")
+              << sortOrder << " " << sortParameter << "\n\n";
+
+    std::cout << "Quick Sort time: " << qt.count() << " seconds\n";
+    std::cout << "Merge Sort time: " << mt.count() << " seconds\n\n";
+
+    //Quick Sort Results
+    std::cout << "Quick Sort Results\n";
+    for (int i = 0; i < (int)quickResults.size(); ++i) {
+        auto &s = quickResults[i];
+        std::cout << (i+1) << ". " << s.trackName
+                  << " by " << s.artistName << " — ";
+        if (sortParameter == "popularity")        std::cout << "Popularity: " << s.popularity << "\n";
+        else if (sortParameter == "danceability") std::cout << "Danceability: " << s.danceability << "\n";
+        else if (sortParameter == "liveness")     std::cout << "Liveness: " << s.liveness << "\n";
+        else /* duration */                        std::cout << "Duration: " << s.duration_ms << " ms\n";
     }
 
-    std::cout<<"sorted by "<<sortParameter<<std::endl;
-
-    std::cout<<"Quick Sort time: "<<quicksortTime.count()<<"seconds"<<std::endl;
-    std::cout<<"Merge Sort time: "<<mergesortTime.count()<<"seconds"<<std::endl;
-
-    std::cout<<"Quick Sort Results" << std::endl;
-    for (int i = 0; i < quickResults.size(); i++) {
-        std::cout<<i + 1<<". ";
-        std::cout<<quickResults[i].trackName;
-
-        if(sortParameter == "popularity") {
-            std::cout<<"Popularity: "<< quickResults[i].popularity<<std::endl;
-        } else if(sortParameter == "liveness") {
-            std::cout<<"Liveness: "<< quickResults[i].liveness<<std::endl;
-        }
+    //Merge Sort Results
+    std::cout << "\nMerge Sort Results\n";
+    for (int i = 0; i < numberOfQueriedSongs; ++i) {
+        auto &s = mergeResults[i];
+        std::cout << (i+1) << ". " << s.trackName
+                  << " by " << s.artistName << " — ";
+        if (sortParameter == "popularity")        std::cout << "Popularity: " << s.popularity << "\n";
+        else if (sortParameter == "danceability") std::cout << "Danceability: " << s.danceability << "\n";
+        else if (sortParameter == "liveness")     std::cout << "Liveness: " << s.liveness << "\n";
+        else std::cout << "Duration: " << s.duration_ms << " ms\n";
     }
 
-    std::cout<<"Merge Sort Results"<<std::endl;
-    for (int i = 0; i < numberOfQueriedSongs; i++) {
-        std::cout<<i + 1<<". ";
-        std::cout<<mergeResults[i].trackName;
-
-        if(sortParameter == "popularity") {
-            std::cout<<"Popularity: "<< mergeResults[i].popularity<<std::endl;
-        } else if(sortParameter == "liveness") {
-            std::cout<<"Liveness: "<< mergeResults[i].liveness<<std::endl;
-        }
-    }
+    return 0;
 }
-
--- musicData.h
+++ musicData.h
@@ -1,6 +1,5 @@
-//
+// musicData.h
 // Created by lchin on 4/20/2025.
-//
 
 #include <fstream>
 #include <iostream>
@@ -12,13 +11,13 @@ using namespace std;
 #define MUSICDATA_H
 
 struct song {
-    //Added a default constructor to song
     song() = default;
 
     std::string artistName;
     std::string trackName;
     std::string trackGenre;
     float popularity;
+    float duration_ms;
     float danceability;
     float liveness;
 
@@ -26,12 +25,14 @@ struct song {
          const std::string &trackName,
          const std::string &trackGenre,
          float popularity,
+         float duration_ms,
          float danceability,
          float liveness)
         : artistName(artistName),
           trackName(trackName),
           trackGenre(trackGenre),
           popularity(popularity),
+          duration_ms(duration_ms),
           danceability(danceability),
           liveness(liveness) {
     }
@@ -49,144 +50,190 @@ public:
                 row["track_name"].get<>(),
                 row["track_genre"].get<>(),
                 row["popularity"].get<float>(),
+                row["duration_ms"].get<float>(),
                 row["danceability"].get<float>(),
                 row["liveness"].get<float>()
             );
         }
     }
 
-    //2 Quick sort functions, one for sorting by liveness, the other for popularity
-    std::vector<song> quick_sort_by_liveness(int N) {
-        std::vector<song> arr = data;
-        quick_sort_liveness(arr.begin(), arr.end());
-        if ((int) arr.size() > N) arr.resize(N);
-        return arr;
-    }
-
     std::vector<song> quick_sort_by_popularity(int N) {
-        std::vector<song> arr = data;
+        auto arr = data;
         quick_sort_popularity(arr.begin(), arr.end());
-        if ((int) arr.size() > N) arr.resize(N);
+        if ((int)arr.size() > N) arr.resize(N);
         return arr;
     }
-
+    std::vector<song> quick_sort_by_danceability(int N) {
+        auto arr = data;
+        quick_sort_danceability(arr.begin(), arr.end());
+        if ((int)arr.size() > N) arr.resize(N);
+        return arr;
+    }
+    std::vector<song> quick_sort_by_liveness(int N) {
+        auto arr = data;
+        quick_sort_liveness(arr.begin(), arr.end());
+        if ((int)arr.size() > N) arr.resize(N);
+        return arr;
+    }
+    std::vector<song> quick_sort_by_duration(int N) {
+        auto arr = data;
+        quick_sort_duration(arr.begin(), arr.end());
+        if ((int)arr.size() > N) arr.resize(N);
+        return arr;
+    }
 
     vector<song> mergeSort(std::vector<song>& songs, string& P) {
-        if(songs.size() <= 1) {
-            return songs;
-        }
-
+        if (songs.size() <= 1) return songs;
         int Mid = songs.size() / 2;
-
         vector<song> L(songs.begin(), songs.begin() + Mid);
         vector<song> R(songs.begin() + Mid, songs.end());
-
-        vector<song> SL = mergeSort(L,P);
-        vector<song> SR = mergeSort(R,P);
+        auto SL = mergeSort(L, P);
+        auto SR = mergeSort(R, P);
         return merge(SL, SR, P);
+    }
+
+    vector<song> quick_sort_subset(const vector<song>& subset,
+                               const string& P,
+                               int N)
+    {
+        auto arr = subset;
+        if      (P == "popularity")   quick_sort_popularity(arr.begin(), arr.end());
+        else if (P == "danceability") quick_sort_danceability(arr.begin(), arr.end());
+        else if (P == "liveness")     quick_sort_liveness(arr.begin(), arr.end());
+        else if (P == "duration")     quick_sort_duration(arr.begin(), arr.end());
+        if ((int)arr.size() > N) arr.resize(N);
+        return arr;
     }
 
 private:
     vector<song> merge(vector<song>& L, vector<song>& R, string& P) {
         vector<song> result;
-        int i,j;
-        i=0;
-        j=0;
-
-        while(i < L.size() && j < R.size()) {
-            if(P == "popularity") {
-                if(L[i].popularity > R[j].popularity) {
-                    result.push_back(L[i]);
-                    i++;
-                }
-                else {
-                    result.push_back(R[j]);
-                    j++;
-                }
-            } else if(P == "liveness") {
-                if(L[i].liveness > R[j].liveness) {
-                    result.push_back(L[i]);
-                    i++;
-                }
-                else {
-                    result.push_back(R[j]);
-                    j++;
-                }
+        int i = 0, j = 0;
+        while (i < (int)L.size() && j < (int)R.size()) {
+            if (P == "popularity") {
+                if (L[i].popularity   > R[j].popularity)   result.push_back(L[i++]);
+                else                                     result.push_back(R[j++]);
+            }
+            else if (P == "danceability") {
+                if (L[i].danceability > R[j].danceability) result.push_back(L[i++]);
+                else                                     result.push_back(R[j++]);
+            }
+            else if (P == "liveness") {
+                if (L[i].liveness     > R[j].liveness)     result.push_back(L[i++]);
+                else                                     result.push_back(R[j++]);
+            }
+            else if (P == "duration") {
+                if (L[i].duration_ms   > R[j].duration_ms) result.push_back(L[i++]);
+                else                                     result.push_back(R[j++]);
             }
         }
-
-        while(i < L.size()) {
-            result.push_back(L[i]);
-            i++;
-        }
-        while(j < R.size()) {
-            result.push_back(R[j]);
-            j++;
-        }
-
+        while (i < (int)L.size()) result.push_back(L[i++]);
+        while (j < (int)R.size()) result.push_back(R[j++]);
         return result;
     }
 
-    //template to allow any type of data to be passed through
-    //both versions of sort use median of three method to choose a pivot
-    template<typename RandomIt>
-    void quick_sort_liveness(RandomIt first, RandomIt last) {
-        if (last - first < 2) return;
-        auto midIt = first + (last - first) / 2;
-        song pivot = *first;
-        song midElem = *midIt;
-        song lastElem = *(last - 1);
-        if (first->liveness > midElem.liveness) {
-            if (midElem.liveness > lastElem.liveness) pivot = midElem;
-            else if (first->liveness > lastElem.liveness) pivot = lastElem;
-            else pivot = *first;
-        } else {
-            if (first->liveness > lastElem.liveness) pivot = *first;
-            else if (midElem.liveness > lastElem.liveness) pivot = lastElem;
-            else pivot = midElem;
-        }
-        RandomIt left = first, right = last - 1;
-        while (left <= right) {
-            while (left->liveness > pivot.liveness) ++left;
-            while (pivot.liveness > right->liveness) --right;
-            if (left <= right) {
-                std::iter_swap(left, right);
-                ++left;
-                --right;
-            }
-        }
-        quick_sort_liveness(first, right + 1);
-        quick_sort_liveness(left, last);
-    }
 
-    template<typename RandomIt>
-    void quick_sort_popularity(RandomIt first, RandomIt last) {
+    template<typename AnyData>
+    void quick_sort_popularity(AnyData first, AnyData last) {
         if (last - first < 2) return;
-        auto midIt = first + (last - first) / 2;
-        song pivot = *first;
-        song midElem = *midIt;
+        auto midIt    = first + (last - first)/2;
+        song pivot    = *first;
+        song midElem  = *midIt;
         song lastElem = *(last - 1);
         if (first->popularity > midElem.popularity) {
             if (midElem.popularity > lastElem.popularity) pivot = midElem;
             else if (first->popularity > lastElem.popularity) pivot = lastElem;
-            else pivot = *first;
         } else {
             if (first->popularity > lastElem.popularity) pivot = *first;
             else if (midElem.popularity > lastElem.popularity) pivot = lastElem;
             else pivot = midElem;
         }
-        RandomIt left = first, right = last - 1;
+        AnyData left = first, right = last - 1;
         while (left <= right) {
             while (left->popularity > pivot.popularity) ++left;
             while (pivot.popularity > right->popularity) --right;
-            if (left <= right) {
-                std::iter_swap(left, right);
-                ++left;
-                --right;
-            }
+            if (left <= right) { std::iter_swap(left, right); ++left; --right; }
         }
         quick_sort_popularity(first, right + 1);
         quick_sort_popularity(left, last);
+    }
+
+
+    template<typename AnyData>
+    void quick_sort_danceability(AnyData first, AnyData last) {
+        if (last - first < 2) return;
+        auto midIt    = first + (last - first)/2;
+        song pivot    = *first;
+        song midElem  = *midIt;
+        song lastElem = *(last - 1);
+        if (first->danceability > midElem.danceability) {
+            if (midElem.danceability > lastElem.danceability) pivot = midElem;
+            else if (first->danceability > lastElem.danceability) pivot = lastElem;
+        } else {
+            if (first->danceability > lastElem.danceability) pivot = *first;
+            else if (midElem.danceability > lastElem.danceability) pivot = lastElem;
+            else pivot = midElem;
+        }
+        AnyData left = first, right = last - 1;
+        while (left <= right) {
+            while (left->danceability > pivot.danceability) ++left;
+            while (pivot.danceability > right->danceability) --right;
+            if (left <= right) { std::iter_swap(left, right); ++left; --right; }
+        }
+        quick_sort_danceability(first, right + 1);
+        quick_sort_danceability(left, last);
+    }
+
+
+    template<typename AnyData>
+    void quick_sort_liveness(AnyData first, AnyData last) {
+        if (last - first < 2) return;
+        auto midIt    = first + (last - first)/2;
+        song pivot    = *first;
+        song midElem  = *midIt;
+        song lastElem = *(last - 1);
+        if (first->liveness > midElem.liveness) {
+            if (midElem.liveness > lastElem.liveness) pivot = midElem;
+            else if (first->liveness > lastElem.liveness) pivot = lastElem;
+        } else {
+            if (first->liveness > lastElem.liveness) pivot = *first;
+            else if (midElem.liveness > lastElem.liveness) pivot = lastElem;
+            else pivot = midElem;
+        }
+        AnyData left = first, right = last - 1;
+        while (left <= right) {
+            while (left->liveness > pivot.liveness) ++left;
+            while (pivot.liveness > right->liveness) --right;
+            if (left <= right) { std::iter_swap(left, right); ++left; --right; }
+        }
+        quick_sort_liveness(first, right + 1);
+        quick_sort_liveness(left, last);
+    }
+
+
+    template<typename AnyData>
+    void quick_sort_duration(AnyData first, AnyData last) {
+        if (last - first < 2) return;
+        auto midIt    = first + (last - first)/2;
+        song pivot    = *first;
+        song midElem  = *midIt;
+        song lastElem = *(last - 1);
+        if (first->duration_ms > midElem.duration_ms) {
+            if (midElem.duration_ms > lastElem.duration_ms) pivot = midElem;
+            else if (first->duration_ms > lastElem.duration_ms) pivot = lastElem;
+        } else {
+            if (first->duration_ms > lastElem.duration_ms) pivot = *first;
+            else if (midElem.duration_ms > lastElem.duration_ms) pivot = lastElem;
+            else pivot = midElem;
+        }
+        AnyData left = first, right = last - 1;
+        while (left <= right) {
+            while (left->duration_ms > pivot.duration_ms) ++left;
+            while (pivot.duration_ms > right->duration_ms) --right;
+            if (left <= right) { std::iter_swap(left, right); ++left; --right; }
+        }
+        quick_sort_duration(first, right + 1);
+        quick_sort_duration(left, last);
     }
 };
 
